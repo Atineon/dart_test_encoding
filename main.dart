31905261c2a552c0aab422d77971510fd704efe6
Antoine ROBIN <antoine.robin@factorfx.com>
2025-08-21 12:31:56 +0000

    fix(main): print output to string

--- main.dart
+++ main.dart
@@ -10,7 +10,7 @@ Future<void> main(List<String> args) async {
       stdoutEncoding: null,
       stderrEncoding: null);
 
-  print("Output: ${process.stdout}");
-  print("Error: ${process.stderr}");
+  print("Output: ${process.stdout.toString()}");
+  print("Error: ${process.stderr.toString()}");
   print("Exit code: ${process.exitCode}");
 }
