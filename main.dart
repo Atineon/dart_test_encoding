a230ca56c3441ee4e492e89d8c5ae06850a4dd94
Antoine ROBIN <antoine.robin@factorfx.com>
2025-08-21 07:56:40 +0000

    feat(main): trim outputs

--- main.dart
+++ main.dart
@@ -6,7 +6,7 @@ Future<void> main(List<String> args) async {
     "(Get-WMIObject -Class Win32_ComputerSystemProduct).Description"
   ]);
 
-  print("Output: " + process.stdout);
-  print("Error: " + process.stderr);
+  print("Output: " + process.stdout.trim());
+  print("Error: " + process.stderr.trim());
   print("ExitCode: " + process.exitCode.toString());
 }
