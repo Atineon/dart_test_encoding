8fd6950ec23c6125dfaf76b2e2acac8b166fc851
Antoine ROBIN <antoine.robin@factorfx.com>
2025-08-21 12:14:23 +0000

    fix(main): standardization

--- main.dart
+++ main.dart
@@ -10,7 +10,7 @@ Future<void> main(List<String> args) async {
       stdoutEncoding: null,
       stderrEncoding: null);
 
-  print("Out  : ${process.stdout}");
+  print("Output  : ${process.stdout}");
   print("Error: ${process.stderr}");
-  print("ExitCode : ${process.exitCode}");
+  print("Exit code : ${process.exitCode}");
 }
