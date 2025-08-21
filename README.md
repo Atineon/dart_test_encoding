9f440b15443ce5dcc352cb06b4279912854970c2
Antoine ROBIN <antoine.robin@factorfx.com>
2025-08-21 08:27:42 +0000

    feat(main): add charset encoding

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Test Encoding
 
-This repository contains tests related to dart encoding. Use these tests to verify correct handling of various character encodings within the application.
+Ce repo contient des tests liés à l'encodage du langage Dart. Utilisez ces tests pour vérifier la gestion correcte des différents encodages de caractères.
 
 ## Usage
+
+Branches:
+- v1: Script renvoyant uniquement la donnée brut tel que dart la renvoie. (Data depuis PS: utf16LE, data depuis Dart: utf8)
+- v2: Essai sur l'encodage d'utf16LE vers utf16
 
 Run the main test script in this folder to check for encoding issues.
--- main.dart
+++ main.dart
@@ -1,12 +1,22 @@
 import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:charset_converter/charset_converter.dart';
 
 Future<void> main(List<String> args) async {
-  var process = await Process.run("powershell.exe", [
-    "-Command",
-    "(Get-WMIObject -Class Win32_ComputerSystemProduct).Description"
-  ]);
+  var process = await Process.run(
+      "powershell.exe",
+      [
+        "-Command",
+        "(Get-WMIObject -Class Win32_ComputerSystemProduct).Description"
+      ],
+      stdoutEncoding: null,
+      stderrEncoding: null);
 
-  print("Output: " + process.stdout.trim());
-  print("Error: " + process.stderr.trim());
+  Uint8List outputBytes = process.stdout as Uint8List;
+  Uint8List errorBytes = process.stderr as Uint8List;
+
+  print("Output: " + await CharsetConverter.decode("windows1252", outputBytes));
+  print("Error: " + await CharsetConverter.decode("windows1252", errorBytes));
   print("ExitCode: " + process.exitCode.toString());
 }
