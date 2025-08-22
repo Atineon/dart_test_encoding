b316fb4966a4577b7a3e3e12f8f6a380f270af41
Antoine ROBIN <antoine.robin@factorfx.com>
2025-08-22 07:28:14 +0000

    feat(main): comment the error of v3

--- main.dart
+++ main.dart
@@ -1,22 +1,17 @@
 import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:charset_converter/charset_converter.dart';
 
 Future<void> main(List<String> args) async {
-  var process = await Process.run(
-      "powershell.exe",
-      [
-        "-Command",
-        "(Get-WMIObject -Class Win32_ComputerSystemProduct).Description"
-      ],
-      stdoutEncoding: null,
-      stderrEncoding: null);
-
-  var stdout = CharsetConverter.decode("utf-16", process.stdout as Uint8List);
-  var stderr = process.stderr as List<int>;
-
+  // output actuel de l'agent
+  var stdout = "Produit systŠme informatique";
   print("Output: ${stdout}");
-  print("Error: ${stderr}");
-  print("Exit code: ${process.exitCode}");
+
+  // visualisation de l'output en bytes décimal
+  print("Output bytes: ${stdout.codeUnits}");
+  // Comme Dart utilise UTF-16 pour les String, on voit que le 'Š' est codé sur 2 bytes (0x0160 = 352)
+  // Et comme l'utf-8 est codé sur 1 byte, le byte décimal 352 n'est pas recevable en utf-8 et fait planter l'agent.
+  // De plus, même si on enlève l'encoding lors du Process.run, (stdoutEncoding: null) le bytes 352 est transformé en 197 (toujours au dessus de 128 bits).
+  var process = await Process.run('echo', ["Produit systŠme informatique"],
+      stdoutEncoding: null, stderrEncoding: null);
+
+  print("Ouput process: ${process.stdout.toString()}");
 }
